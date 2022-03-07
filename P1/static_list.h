2c2e6ff97ebc87792e533a09882e524c9d9d63e3
brais.papiri <brais.papiri@udc.es>
2022-03-07 12:52:48 +0100

    Inicio static

--- P1/static_list.h
+++ P1/static_list.h
@@ -1,10 +1,10 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 1
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
+ * AUTHOR 1: Brais Fernandez Papiri LOGIN 1: brais.papiri
+ * AUTHOR 2: Samanta Karolay Machuca Montalvo LOGIN 2: s.machucam
+ * GROUP: 4.3
+ * DATE: 11 / 03 / 21
  */
 
 #ifndef STATIC_LIST_H
