ae5823c30e59c4f3c640110f461967d73f22df52
brais.papiri <brais.papiri@udc.es>
2022-04-08 14:40:54 +0200

    Bid stack

--- P2/bid_stack.h
+++ P2/bid_stack.h
@@ -1,17 +1,37 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 2
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
+ * AUTHOR 1: Brais Fernandez Papiri LOGIN 1: brais.papiri
+ * AUTHOR 2: Samanta Karolay Machuca Montalvo LOGIN 2:s.machucam
+ * GROUP: 4.3
+ * DATE: 08 / 04 / 21
  */
 
 #ifndef BID_STACK_H
 #define BID_STACK_H
 
 #include "types.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
-/* Write your code here... */
+#define SNULL NULL
+
+typedef int tItemS;
+typedef struct tNodeS *tPosS;
+
+struct tNodeS {
+    tItemS data;
+    tPosS down;
+};
+
+typedef tPosS tStack;
+
+void createEmptyStack(tStack *S);
+bool createNode(tPosS *p);
+bool push(tItemS d, tStack *S);
+void pop(tStack *S);
+tItemS peek(tStack S);
+bool isEmptyStack(tStack S);
 
 #endif
--- P2/product_list.c
+++ P2/product_list.c
@@ -1,12 +1,13 @@
 /*
  * TITLE: PROGRAMMING II LABS
  * SUBTITLE: Practical 2
- * AUTHOR 1: ***************************** LOGIN 1: **********
- * AUTHOR 2: ***************************** LOGIN 2: **********
- * GROUP: *.*
- * DATE: ** / ** / **
+ * AUTHOR 1: Brais Fernandez Papiri LOGIN 1: brais.papiri
+ * AUTHOR 2: Samanta Karolay Machuca Montalvo LOGIN 2:s.machucam
+ * GROUP: 4.3
+ * DATE: 08 / 04 / 21
  */
 
 #include "product_list.h"
+
 
 /* Write your code here... */
